7e38437da709e5038b9dfd72545505f28c3b346e
lairase <ovidio.lairana@some.com>
2020-08-23 17:30:41 +0200

    misc

--- src/app/common/model/table-options.model.ts
+++ src/app/common/model/table-options.model.ts
@@ -1,3 +1,5 @@
+import { TemplateRef } from '@angular/core';
+
 export class TableOptionsModel {
-    constructor(public field: string, public header: string) {}
+    constructor(public field: string, public header: string, fieldTemplateRef?: TemplateRef<any>, headerTemplateRef?: TemplateRef<any>) {}
 }
